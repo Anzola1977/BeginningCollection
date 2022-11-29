e76accdb98c54a40bba0dd4d2b6b914000a7fe22
Andrew Zola <and.zola.77@gmail.com>
2022-11-29 15:46:04 +0100

    Commit read URL with write info3

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String str = "https://randomuser.me/api";
+        String str = "https";
+        InputStream inputStream;
         try {
             URL url = new URL(str);
-            InputStream inputStream = url.openStream();
+            inputStream = url.openStream();
             FileOutputStream fileOutputStream = new FileOutputStream("people.json");
 
             //for (byte b : inputStream.readAllBytes()){
                 fileOutputStream.write(inputStream.readAllBytes());
 
         } catch (MalformedURLException e) {
-            System.out.println("Incorrect URL");
+            try {
+                inputStream = new FileInputStream("config.txt");
+                //Reader configReader = InputStreamReader(inputStream, StandardCharsets.UTF_8)
+                for (byte b:inputStream.readAllBytes()) {
+                    System.out.print((char)b);
+                }
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         } catch (IOException e) {
             System.out.println("Unable connect to URL");
         }
